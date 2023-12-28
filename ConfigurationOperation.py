dc2e1e75e6b93c94e5e027d71685465ef4e2464a
Juan Perez <jperez@beyondtechnologies.ca>
2023-12-28 15:19:42 -0500

    update

--- ConfigurationOperation.py
+++ ConfigurationOperation.py
@@ -9,22 +9,17 @@ class ConfigurationOperation:
     password = "Oze0i"
     url = "http://www.fxcorporate.com/Hosts.jsp"
     connectiontype = "Demo"
-    instrument = 'EUR/CAD'
     session = None
     pin = None
-    lots = 2
+    lots = 3
     stop = 8
     limit = 16
     account = None
     timeframe = "m1"  # Available periods :  'm1', 'm5', 'm15', 'm30', 'H1', 'H2', 'H3', 'H4', 'H6', 'H8','D1', 'W1', or 'M1'.
-    quotes_count = 200000  #Number of Candles
     dateFormat = '%m.%d.%Y %H:%M:%S'
     date_from =  None
     date_to = None
-    d_buy_sell = None # B(BUY) OR S(SELL)
-
     days = 1
-    filename = None
 
     peggedstop = 'Y'
     peggedlimit = 'Y'
@@ -36,4 +31,3 @@ class ConfigurationOperation:
         europe_London_datetime = datetime.now( timezone('Europe/London') )
         self.date_from =  europe_London_datetime - dt.timedelta(days=self.days)
         self.date_to = europe_London_datetime
-        self.filename =  self.instrument.replace("/", "_")  + "_" + self.timeframe + ".csv"
